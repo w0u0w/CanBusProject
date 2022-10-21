a2139c6a15838337cfa330a7bb322b9de672d630
w0u0w <homerjs92@gmail.com>
2022-10-21 17:48:49 +0300

    check new function

--- CanBusProject/canbusapp/views.py
+++ CanBusProject/canbusapp/views.py
@@ -22,26 +22,16 @@ def createTask(bus, msg):
     return task
 
 
-def genNewMsg():
-    msg = can.Message(arbitration_id=random.randint(1, 100), data=[1, 1, 1, 1])
-    return msg
-
-
 @csrf_exempt
 def vcan0(request):
     bus = can.interface.Bus(interface='socketcan', channel='vcan0')
-    # msg = can.Message(arbitration_id=random.randint(1, 100), data=[1, 1, 1, 1])
-    # myTask = createTask(bus, genNewMsg())
-    bus.send_periodic(genNewMsg(), 2)
+    msg = can.Message(arbitration_id=random.randint(1, 2), data=[1, 1, 1, 1])
+    myTask = createTask(bus, msg)
+    bus.send_periodic(msg, 2)
     if request.POST.get('operation') == 'startsending':
-        pass
-        # myTask.start()
+        myTask.start()
     if request.POST.get('operation') == 'stopsending':
-        bus.stop_all_periodic_tasks()
-        # bus.stop_all_periodic_tasks()
-        # myTask.stop()
-        # bus.shutdown()
-
+        myTask.stop()
     return render(request, "vcan0.html", {'interface': 'vcan0', })
   
   
