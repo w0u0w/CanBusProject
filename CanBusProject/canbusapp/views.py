de7ec99905b40f6923b36e8c641cfd0b19f83700
w0u0w <homerjs92@gmail.com>
2022-10-25 16:20:24 +0300

    try run C program

--- CanBusProject/canbusapp/views.py
+++ CanBusProject/canbusapp/views.py
@@ -17,9 +17,11 @@ def index(request):
 def vcan0(request):
     # p0 = subprocess.Popen(["/home/www/code/sendcanframe", "1", "1"])
     if request.POST.get('operation') == 'startsending':
-        subprocess.call(["./home/www/code/sendcanframe", "1", "1"])
+        subprocess.call(["cd", "/home/www/code"])
+        subprocess.call(["./sendcanframe", "1", "1"])
     if request.POST.get('operation') == 'stopsending':
-        subprocess.call(["./home/www/code/sendcanframe", "1", "0"])
+        subprocess.call(["cd", "/home/www/code"])
+        subprocess.call(["./sendcanframe", "1", "0"])
         # subprocess.Popen(["/home/www/code/sendcanframe", "1", "0"])
     return render(request, "vcan0.html", {'interface': 'vcan0', })
 
