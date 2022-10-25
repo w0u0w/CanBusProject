1de819df49b80d0ebe11417bc020e92f5f180c79
w0u0w <homerjs92@gmail.com>
2022-10-25 12:07:15 +0300

    try run C program

--- CanBusProject/canbusapp/views.py
+++ CanBusProject/canbusapp/views.py
@@ -1,14 +1,10 @@
+import subprocess
+
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from can.interface import Bus
-import subprocess
-import sys
-from threading import Thread
-# Create your views here.
-from django.http import HttpResponse
+from subprocess import call
 import can
 import random
-import time
 
 
 def index(request):
@@ -23,40 +19,36 @@ def createTask(bus, msg):
 
 @csrf_exempt
 def vcan0(request):
-    bus = can.interface.Bus(interface='socketcan', channel='vcan0')
-    msg = can.Message(arbitration_id=random.randint(1, 10), data=[1, 1, 1, 1])
-    myTask = createTask(bus, msg)
-    myTask.stop()
     if request.POST.get('operation') == 'startsending':
-        myTask.start()
+        subprocess.run("./hello")
     if request.POST.get('operation') == 'stopsending':
-        myTask.stop()
+        pass
     return render(request, "vcan0.html", {'interface': 'vcan0', })
-  
-  
+
+
 def vcan1(request):
-  return render(request, "vcan1.html", {'interface': 'vcan1'})
+    return render(request, "vcan1.html", {'interface': 'vcan1'})
 
 
 def vcan2(request):
-  return render(request, "vcan2.html", {'interface': 'vcan2'})
+    return render(request, "vcan2.html", {'interface': 'vcan2'})
 
 
 def vcan3(request):
-  return render(request, "vcan3.html", {'interface': 'vcan3'})
+    return render(request, "vcan3.html", {'interface': 'vcan3'})
 
 
 def vcan4(request):
-  return render(request, "vcan4.html", {'interface': 'vcan4'})
+    return render(request, "vcan4.html", {'interface': 'vcan4'})
 
 
 def vcan5(request):
-  return render(request, "vcan5.html", {'interface': 'vcan5'})
+    return render(request, "vcan5.html", {'interface': 'vcan5'})
 
 
 def vcan6(request):
-  return render(request, "vcan6.html", {'interface': 'vcan6'})
+    return render(request, "vcan6.html", {'interface': 'vcan6'})
 
 
 def vcan7(request):
-  return render(request, "vcan7.html", {'interface': 'vcan7'})
+    return render(request, "vcan7.html", {'interface': 'vcan7'})
