5ee6c754b76753a80b6e66432a9cfbf05e1a0c2c
w0u0w <homerjs92@gmail.com>
2022-12-01 17:11:45 +0300

    paralleling terminal

--- CanBusProject/canbusapp/views.py
+++ CanBusProject/canbusapp/views.py
@@ -15,29 +15,6 @@ def calcBytes(dataFromPage):
     return str(res)
 
 
-@sync_to_async
-def asyncTest(status, tmIndex, dataModuleList):
-    module0 = calcBytes(dataModuleList[0])
-    module1 = calcBytes(dataModuleList[1])
-    module2 = calcBytes(dataModuleList[2])
-    module3 = calcBytes(dataModuleList[3])
-    module4 = calcBytes(dataModuleList[4])
-    module5 = calcBytes(dataModuleList[5])
-    module6 = calcBytes(dataModuleList[6])
-    module7 = calcBytes(dataModuleList[7])
-    p0 = subprocess.Popen(
-        [
-            "/home/www/code/test2",
-            str(tmIndex),
-            "1",
-            "2",
-            module0, module1, module2, module3, module4, module5, module6, module7
-        ])
-    if status == 0:
-        pass
-        os.killpg(os.getpgid(p0.pid), signal.SIGTERM)
-
-
 def base(request):
     return render(request, 'base.html')
 
@@ -59,11 +36,25 @@ async def terminalPage(request, tmIndex):
             status = 0
         if operation == 'dataVcanPost':
             dataModuleList = request.POST.getlist('dataVcan[]')
-            task = asyncio.ensure_future([asyncTest(status, dataModuleList, tmIndex)])
-            await asyncio.wait(task)
+            module0 = calcBytes(dataModuleList[0])
+            module1 = calcBytes(dataModuleList[1])
+            module2 = calcBytes(dataModuleList[2])
+            module3 = calcBytes(dataModuleList[3])
+            module4 = calcBytes(dataModuleList[4])
+            module5 = calcBytes(dataModuleList[5])
+            module6 = calcBytes(dataModuleList[6])
+            module7 = calcBytes(dataModuleList[7])
+            p0 = subprocess.Popen(
+                [
+                    "/home/www/code/test2",
+                    str(tmIndex),
+                    "1",
+                    "2",
+                    module0, module1, module2, module3, module4, module5, module6, module7
+                ])
             if status == 0:
                 pass
-                # os.killpg(os.getpgid(p0.pid), signal.SIGTERM)
+                os.killpg(os.getpgid(p0.pid), signal.SIGTERM)
     with open("/home/www/code/data2.txt") as f:
         for line in f:
             dataFile.append(line.strip())
